ebfbf878085a5871aa389ef165fa8f372412e5be
Just <just8@mail.ru>
2019-02-05 12:55:32 +0300

    Адаптивная верстка макета странички.

---
+++ src/assets/js/app.js
@@ -0,0 +1,97 @@
+$(function() {
+    "use strict";
+
+    // Тестируем, что js-скрипты попали в сборку.
+    const logger = (word = "Hi,") => {
+        console.log(word, "🤨!!!", "\nThank you for using this template!😎");
+    };
+    logger();
+
+    // Метод переключающий текст в блоках.
+    $.fn.extend({
+        toggleText: function(a, b){
+            return this.text(this.text() == b ? a : b);
+        }
+    });
+
+    // Делаем упаковки активными, при клике на нее(них).
+    $(".funbox__item").on({
+        mousedown: function () {
+            let parent = $(this).closest(".funbox__item-main");
+
+            $(this).toggleClass("funbox__item_active");
+            /*$(".funbox__item").on("mouseenter",function(){
+             $(this).toggleClass("funbox__item_active");
+             });*/
+            $(this).find(".funbox__weight").toggleClass("funbox__weight_active");
+
+            $(parent).find(".funbox__text-first").toggleClass("funbox__text_hide");
+            $(parent).find(".funbox__text-two").toggleClass("funbox__text_hide");
+
+            $(this).find(".funbox__heading1").removeClass("funbox__heading1_active").text("Сказочное заморское яство");
+
+        },
+        mouseup: function () {
+            //$(".funbox__item").off("mouseenter");
+        }
+    });
+
+    // Делаем упаковки активными, при клике по ссылке "Купи".
+    $(".funbox__link").on( "click", function(event) {
+        event.preventDefault();
+
+        let parent = $(this).closest(".funbox__item-main");
+
+        $(parent).find(".funbox__item").toggleClass("funbox__item_active");
+        $(parent).find(".funbox__weight").toggleClass("funbox__weight_active");
+
+        $(parent).find(".funbox__text-first").toggleClass("funbox__text_hide");
+        $(parent).find(".funbox__text-two").toggleClass("funbox__text_hide");
+
+        $(parent).find(".funbox__heading1").removeClass("funbox__heading1_active").text("Сказочное заморское яство");
+        $(this).find(".funbox__heading1").removeClass("funbox__heading1_active");
+
+    });
+
+    // При отведении мышки от выбранной упаковки, меняем текст в заголовке. И цвет данного текста.
+    $( ".funbox__item" ).on({
+        mouseleave: function(){
+
+            let _self = this;
+
+            function change_text() {
+                if($(_self).hasClass("funbox__item_active")) {
+                    $(_self).find(".funbox__heading1").addClass("funbox__heading1_active");
+                    //$(_self).find(".funbox__heading1").toggleText("Сказочное заморское яство", "Котэ не одобряет?");
+                    $(_self).find(".funbox__heading1").text("Котэ не одобряет?");
+                }
+            }
+
+            setTimeout(change_text, 100);
+        }
+    });
+
+    // При клике по ссылке "Но можно проверить" под третьей упаковкой убираем ее неактивность. И удаляем указанную ссылку.
+    $(".funbox__link-switch").on( "click", function(event) {
+        event.preventDefault();
+
+        let parent = $(this).closest(".funbox__item-main");
+
+        $(parent).find(".funbox__item").removeClass("funbox__item_disabled");
+        $(parent).find(".funbox__item-inner").removeClass("funbox__item-inner_disabled");
+        $(parent).find(".funbox__heading1").removeClass("funbox__heading1_disabled");
+        $(parent).find(".funbox__heading2").removeClass("funbox__heading2_disabled");
+        $(parent).find(".funbox__heading3").removeClass("funbox__heading3_disabled");
+        $(parent).find(".funbox__description").removeClass("funbox__description_disabled");
+        $(parent).find(".funbox__weight").removeClass("funbox__weight-disabled");
+
+        $(parent).find(".funbox__text-disabled").detach();
+        $(parent).find(".funbox__text-first").removeClass("funbox__text_hide");
+        
+        $(parent).find(".funbox__heading1").removeClass("funbox__heading1_active").text("Сказочное заморское яство");
+        $(this).find(".funbox__heading1").removeClass("funbox__heading1_active");
+
+        $(this).detach();
+    });
+
+});
